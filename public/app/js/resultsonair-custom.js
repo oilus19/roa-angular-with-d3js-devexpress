50d5fb58907ed003325eb99f8eb2061d1d9056e5
oilus <oilus20@protonmail.com>
2015-10-21 02:59:57 +0900

    Export to Excel updated

--- public/app/js/resultsonair-custom.js
+++ public/app/js/resultsonair-custom.js
@@ -887,6 +887,8 @@ function between(randNumMin, randNumMax)
 	return randInt;
 }
 
+
+
 function exportTableToCSV($table, filename) {
 	var $ = jQuery;
     var $rows = $table.find('tr:has(td,th)'),
@@ -924,6 +926,74 @@ function exportTableToCSV($table, filename) {
         .attr({
         'download': filename,
             'href': csvData,
+            'target': '_blank'
+    });
+}
+
+
+function exportTableToExcel($table, filename) {
+	var $ = jQuery;
+    var excel="<table>";
+	// Header
+	$table.find('thead').find('tr').each(function() {
+		excel += "<tr>";
+		$(this).filter(':visible').find('th').each(function(index,data) {
+			if ($(this).css('display') != 'none'){				
+				excel += "<td>" + $(this).text().replace(/"/g, '""')+ "</td>";
+			}
+		});	
+		excel += '</tr>';						
+		
+	});					
+	
+	
+	// Row Vs Column
+	var rowCount=1;
+	$table.find('tbody').find('tr').each(function() {
+		excel += "<tr>";
+		var colCount=0;
+		$(this).filter(':visible').find('td').each(function(index,data) {
+			if ($(this).css('display') != 'none'){	
+				excel += "<td>"+$(this).text().replace(/"/g, '""')+"</td>";
+			}
+			colCount++;
+		});															
+		rowCount++;
+		excel += '</tr>';
+	});					
+	excel += '</table>'
+	
+	var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'>";
+	excelFile += "<head>";
+	excelFile += "<!--[if gte mso 9]>";
+	excelFile += "<xml>";
+	excelFile += "<x:ExcelWorkbook>";
+	excelFile += "<x:ExcelWorksheets>";
+	excelFile += "<x:ExcelWorksheet>";
+	excelFile += "<x:Name>";
+	excelFile += "{worksheet}";
+	excelFile += "</x:Name>";
+	excelFile += "<x:WorksheetOptions>";
+	excelFile += "<x:DisplayGridlines/>";
+	excelFile += "</x:WorksheetOptions>";
+	excelFile += "</x:ExcelWorksheet>";
+	excelFile += "</x:ExcelWorksheets>";
+	excelFile += "</x:ExcelWorkbook>";
+	excelFile += "</xml>";
+	excelFile += "<![endif]-->";
+	excelFile += "</head>";
+	excelFile += "<body>";
+	excelFile += excel;
+	excelFile += "</body>";
+	excelFile += "</html>";
+
+	var base64data = "base64," + $.base64.encode(excelFile);
+	var excelData = "data:application/vnd.ms-excel;filename=exportData.doc;" + base64data;
+
+    $(this)
+        .attr({
+        'download': filename,
+            'href': excelData,
             'target': '_blank'
     });
 }
